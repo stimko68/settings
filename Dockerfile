8db18be86f975ead9042b59b7432bd24201ef69f
Nick Simko <nick.simko@datarobot.com>
2020-04-10 11:22:25 -0600

    * delete the Dockerfile since it's not needed * Got the run_tests script to run all tests correctly

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM python:3.8-alpine
-
-ENV PYTHONBUFFERED=1
-
-COPY requirements.txt .
-
-RUN pip3 install -U -r requirements.txt
--- run_tests.sh
+++ run_tests.sh
@@ -1,29 +1,44 @@
 #!/usr/bin/env bash
-# Run tests against the repo
+# Primary entrypoint for running tests against the repo
 
 set -euo pipefail
 
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 USE_DOCKER="${USE_DOCKER:-true}"
 
-function run_shellcheck() {
-    local cmd="shellcheck -x bin/* *.sh"
-    docker run -i -v "${BASE_DIR}":/data koalaman/shellcheck-alpine sh -c 'cd /data && '
-}
-
-function run_pylint() {
-    printf "pip3 install -U -r requirements.txt \npylint --msg-template=\"{path}:{line}:{column}:{C}:({symbol}){msg}\" pythonlib\n" > TEST_SCRIPT
+function run_wrapper() {
+    local run_cmd="${1}"
+    local docker_image="${2:-}"
 
     if [[ "${USE_DOCKER}" == "true" ]]; then
-        docker run -i -v "${BASE_DIR}":/data python:3.8 bash -c 'cd /data && bash TEST_SCRIPT'
+        run_cmd="cd /data && ${run_cmd}"
+        docker run -i --rm -v "${BASE_DIR}":/data "${docker_image}" sh -c "${run_cmd}"
     else
-        bash TEST_SCRIPT
+        eval "${run_cmd}"
     fi
 }
 
+function run_shellcheck() {
+    echo "Running shellcheck"
+    run_wrapper "shellcheck -x bin/* *.sh" koalaman/shellcheck-alpine
+}
+
+function run_pylint() {
+    echo "Running pylint"
+    run_wrapper 'pip3 install -U -r requirements.txt && pylint --msg-template="{path}:{line}:{column}:{C}:({symbol}){msg}" pythonlib' python:3.8
+}
+
 function run_all() {
-    run_shellcheck
-    run_pylint
+    echo "Running all tests"
+    local failed_tests=()
+
+    run_shellcheck || failed_tests+=("Shellcheck")
+    run_pylint || failed_tests+=("pylint")
+
+    if [[ "${#failed_tests[@]}" -gt 0 ]]; then
+        echo "FAILED TESTS: ${failed_tests[*]}"
+        exit 1
+    fi
 }
 
 function cleanup() {
@@ -34,18 +49,18 @@ while test $# -gt 0; do
     _key="$1"
     case "$_key" in
         --pylint)
-            echo 'Running pylint tests'
             run_pylint
             shift;;
         --shellcheck)
-            echo 'Running shellcheck'
             run_shellcheck
             shift;;
         --)
             shift
+            run_all
             break;;
         *)
-            run_all
+            echo "ERROR: invalid option"
+            exit 1
             ;;
     esac
 done
